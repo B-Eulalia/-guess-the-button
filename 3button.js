2f0d9c71ade1d7953824085b0219ab6f61f7a3d2
B-Eulalia <101184278+B-Eulalia@users.noreply.github.com>
2022-04-06 17:39:35 +0300

    Update 3button.js

--- 3button.js
+++ 3button.js
@@ -8,6 +8,6 @@ function button(id) {
           if (randomButton == id) {
                     document.getElementById("display").innerHTML = "Congratulations!!! You are the winner!";
           } else {
-          document.getElementById("display").innerHTML = "Sorry! You have to try again!";
+                    document.getElementById("display").innerHTML = "Sorry! You have to try again!";
           }
 }
